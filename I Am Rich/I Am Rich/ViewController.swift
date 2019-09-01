f3dae287832b86d02fba7c68d6aaf918d40daef7
vovanbechin <bechin.vovan@gmail.com>
2019-09-01 18:21:30 +0900

    Develop ios 20190901

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by bechin vovan on 2019/09/01.
+//  Copyright © 2019 bechinvovan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
