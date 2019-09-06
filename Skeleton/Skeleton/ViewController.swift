333422f7db9ab08336eeac874b6ed14b5c3ec02a
vovanbechin <bechin.vovan@gmail.com>
2019-09-06 21:26:41 +0900

    Finish Xylophone

---
+++ Skeleton/Skeleton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skeleton
+//
+//  Created by bechin vovan on 2019/09/05.
+//  Copyright © 2019 bechinvovan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
